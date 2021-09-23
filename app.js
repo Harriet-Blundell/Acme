9d099245b0824d37ac66153acdada113c97a161f
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2021-09-23 19:59:38 +0100

    Set up css

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const engine = require('consolidate');
 const { getPage } = require("./controllers/page.controller.js");
 
 const app = express();
+app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 app.engine('html', engine.mustache);
 app.set('view engine', 'html');
--- error/error.js
+++ error/error.js
@@ -1,5 +1,5 @@
 function routeError(res) {
-    res.status(404).send("<h1>Page not found on the server</h1>");
+    res.status(404).render('404_error_template', { message: "Sorry, page not found" });
 }
 
 module.exports = routeError;
